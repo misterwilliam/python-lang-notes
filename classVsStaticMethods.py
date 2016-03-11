e5717221ffdc14c1a4b8d203bc003d70d937fd01
William Chang <mr.williamchang@gmail.com>
2016-03-11 14:43:08 -0800

    Add output

--- classVsStaticMethods.py
+++ classVsStaticMethods.py
@@ -38,3 +38,19 @@ ChildNoOverrider.staticMethod()
 ChildNoOverrider.classMethod()
 c2.staticMethod()
 c2.classMethod()
+
+# Output
+# Parent static method
+# Parent class method: <class '__main__.Parent'>
+# Parent static method
+# Parent class method: <class '__main__.Parent'>
+# ChildOverrider static method
+# ChildOverrider class method: <class '__main__.ChildOverrider'>
+# ChildOverrider static method
+# ChildOverrider class method: <class '__main__.ChildOverrider'>
+# Parent static method
+# Parent class method: <class '__main__.ChildNoOverrider'>
+# Parent static method
+# Parent class method: <class '__main__.ChildNoOverrider'>
+
+# staticmethods behave exactly like classmethods if you ignore the cls argument.
